010ab30b084a2dc09d2751dfc16a1db32aac5c32
JoshProg96 <165125348+JoshProg96@users.noreply.github.com>
2025-12-04 12:21:10 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
-self.addEventListener('install', e => self.skipWaiting()); self.addEventListener('activate', e => self.clients.claim());
+const CACHE_NAME = 'qr-scanner-v1';
+const ASSETS = ['./','./index.html','./manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{}));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
