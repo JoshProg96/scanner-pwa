bb195b2f2dda69e9b2e92c66ab607003952046c1
JoshProg96 <165125348+JoshProg96@users.noreply.github.com>
2025-12-04 12:40:16 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,18 @@
-const CACHE_NAME = 'qr-scanner-v1';
-const ASSETS = ['./','./index.html','./manifest.json'];
+const CACHE_NAME = 'qr-pwa-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{}));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{})
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
 });
